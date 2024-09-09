e3b4ba3eaa072f21c8128ae9d5fb429843c75f19
WT-HOANGMINH <153536086+WT-HOANGMINH@users.noreply.github.com>
2024-09-10 00:09:23 +0700

    Add files via upload

---
+++ processor.java
@@ -0,0 +1,18 @@
+
+package assignment.pkg10;
+
+import java.awt.print.Book;
+import java.text.ParseException;
+
+public class processor {
+
+
+public class Processor {
+    public static void main(String[] args) throws ParseException {
+        Book book1 = new Book();
+        System.out.println(book1.toString());
+    }
+
+    }
+    
+}
